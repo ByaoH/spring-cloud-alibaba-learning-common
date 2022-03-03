422acd36b1e47524506b4b608cca2675d70fdee5
luliangyu <luliangyu@seewintech.com>
2022-03-03 14:13:28 +0800

    修改版本 新增 id生成器

---
+++ src/main/java/com/byaoh/cloud/common/base/SnowflakeGenerator.java
@@ -0,0 +1,24 @@
+package com.byaoh.cloud.common.base;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+
+/**
+ * hibernate 雪花id生成器
+ *
+ * @author luliangyu
+ * @date 2022-03-03 14:02
+ */
+public class SnowflakeGenerator implements IdentifierGenerator {
+	private static final Snowflake SNOWFLAKE = IdUtil.getSnowflake();
+
+	@Override
+	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+		return SNOWFLAKE.nextId();
+	}
+}
