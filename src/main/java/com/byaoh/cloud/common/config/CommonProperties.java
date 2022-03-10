87c9d7da8dabee469fb4613954f71d61b3e24ddb
L7-BUG <ll789@gmail.com>
2022-03-10 22:27:23 +0800

    自定义starter

--- src/main/java/com/byaoh/cloud/common/config/CommonProperties.java
+++ src/main/java/com/byaoh/cloud/common/config/CommonProperties.java
@@ -1,11 +1,21 @@
 package com.byaoh.cloud.common.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * Common
  *
  * @author luliangyu
  * @date 2022-03-10 18:04
  */
-@ConfigurationProperties(prefix = "demo")
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "common")
 public class CommonProperties {
+	/**
+	 * 测试
+	 */
+	private String test = "COMMON-STARTER";
 }
--- src/main/java/com/byaoh/cloud/common/config/CommonRedisConfig.java
+++ src/main/java/com/byaoh/cloud/common/config/CommonRedisConfig.java
@@ -1,10 +1,19 @@
 package com.byaoh.cloud.common.config;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * RedisConfig
  *
  * @author luliangyu
  * @date 2022-03-10 17:38
  */
+@Slf4j
 public class CommonRedisConfig {
+	private final CommonProperties commonProperties;
+
+	public CommonRedisConfig(CommonProperties commonProperties) {
+		this.commonProperties = commonProperties;
+		log.error(commonProperties.getTest());
+	}
 }
