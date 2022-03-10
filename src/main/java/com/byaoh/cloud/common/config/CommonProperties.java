992ad8a4cd612f937115ba43cabe7c435e8327d9
luliangyu <luliangyu@seewintech.com>
2022-03-10 18:17:26 +0800

    修改pom

---
+++ src/main/java/com/byaoh/cloud/common/config/CommonProperties.java
@@ -0,0 +1,11 @@
+package com.byaoh.cloud.common.config;
+
+/**
+ * Common
+ *
+ * @author luliangyu
+ * @date 2022-03-10 18:04
+ */
+@ConfigurationProperties(prefix = "demo")
+public class CommonProperties {
+}
---
+++ src/main/java/com/byaoh/cloud/common/config/CommonRedisConfig.java
@@ -0,0 +1,10 @@
+package com.byaoh.cloud.common.config;
+
+/**
+ * RedisConfig
+ *
+ * @author luliangyu
+ * @date 2022-03-10 17:38
+ */
+public class CommonRedisConfig {
+}
