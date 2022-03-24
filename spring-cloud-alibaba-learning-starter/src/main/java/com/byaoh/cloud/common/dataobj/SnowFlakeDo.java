23b24e1c0908e04863e879a4194a21274c34a69d
L7-BUG <ll789@gmail.com>
2022-03-24 23:05:54 +0800

    增加 get set 注解

--- spring-cloud-alibaba-learning-starter/src/main/java/com/byaoh/cloud/common/dataobj/SnowFlakeDo.java
+++ spring-cloud-alibaba-learning-starter/src/main/java/com/byaoh/cloud/common/dataobj/SnowFlakeDo.java
@@ -1,5 +1,7 @@
 package com.byaoh.cloud.common.dataobj;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
  * @author l
  * @date 2022/3/24 下午11:02
  */
+@Getter
+@Setter
 public class SnowFlakeDo extends BaseDo {
 	private static final long serialVersionUID = 4541431227446057357L;
 	@Id
